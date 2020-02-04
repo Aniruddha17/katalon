4ec5c66d39d2e1f881374a66bc1580247bcc6a14
Aniruddha <aniruddha.mayekar@ext.saint-gobain.com>
2020-02-05 01:50:02 +0530

    Refresh

---
+++ Test Suites/Open browser.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Open browser</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b1b30882-e474-4507-b881-c449c49f7b12</testSuiteGuid>
+   <testCaseLink>
+      <guid>dc5bef51-7db5-42e9-8b23-e1be6484555a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
